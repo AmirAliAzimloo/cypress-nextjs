23ec2ce4f2032ed25f2bfd49ae6d3a3ab2735c58
AmirAliAzimloo <amiraliazimloo123@gmail.com>
2024-04-19 23:20:39 +0430

    Add E2E tests.

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,5 +1,24 @@
 describe('navigation tests', () => {
-  it('passes', () => {
-    cy.visit('/')
+  it('tests opening and closing sidebar', () => {
+    cy.get('[data-id="open-sidebar"]').should('not.exist');
+    cy.visit('/');
+    cy.get('[data-id="open-sidebar-btn"]').click();
+    cy.get('[data-id="open-sidebar"]').should('be.visible');
+    cy.get('[data-id="close-sidebar-btn"]').click();
+    cy.get('[data-id="open-sidebar"]').should('not.exist');
+  });
+  
+  it.only('tests navigation', () => {
+    cy.visit('/');
+    cy.get('[data-id="open-sidebar-btn"]').click();
+
+    cy.contains(/about me/i).should('not.exist');
+    
+    cy.get('[data-id="about-link"]').click();
+    
+    cy.get('[data-id="open-sidebar"]').should('not.exist');
+
+    cy.contains(/about me/i);
   })
+
 })
